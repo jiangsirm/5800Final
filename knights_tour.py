86184e9c4fbb526d98073822c543517172421b9a
jiangsirm <jycsirm@gmail.com>
2024-08-01 18:35:45 -0700

    workingCode

--- knights_tour.py
+++ knights_tour.py
@@ -1,48 +1,60 @@
+import sys
+
 # Python3 program to solve Knight Tour problem using Backtracking
-
 # Chessboard Size
-# comment 1
-# This is a comment
-n = 8
+# run the program by entering "python knights_tour.py <width> <height>" in terminal
 
 
-def isSafe(x, y, board):
-    '''
-        A utility function to check if i,j are valid indexes
-        for N*N chessboard
-    '''
-    if 0 <= x < n and 0 <= y < n and board[x][y] == -1:
+# check for bounds of the movable positions
+def isSafe(x, y, board, width, height):
+    """
+        check for bounds and occupancy of the movable positions'
+
+        :param x: the vertical index of the position
+        :param y: the horizontal index of the position
+        :param board: the 2D board for the problem
+        :param width: width of the board
+        :param height: the height of the board
+        :return: a boolean value. If true that position can be moved to, else the position is not available.
+    """
+
+    if 0 <= x < height and 0 <= y < width and board[x][y] == -1:
         return True
     return False
 
 
-def printSolution(n, board):
-    '''
-        A utility function to print Chessboard matrix
-    '''
-    for i in range(n):
-        for j in range(n):
+# function to print out the board if there is a solution
+def printSolution(width, height, board):
+    """
+        function to print out the board if there is a solution
+
+        :param width: width of the board
+        :param height: the height of the board
+        :param board: the 2D board array to be printed
+        :return: None. It would print the board.
+    """
+    for i in range(height):
+        for j in range(width):
             print(board[i][j], end=' ')
         print()
 
 
-def solveKT(n):
-    '''
-        This function solves the Knight Tour problem using
-        Backtracking. This function mainly uses solveKTUtil()
-        to solve the problem. It returns false if no complete
-        tour is possible, otherwise return true and prints the
-        tour.
-        Please note that there may be more than one solutions,
-        this function prints one of the feasible solutions.
-    '''
+# the outer function for backtracking
+def solveKT(width, height):
+    """
+        the outer function for backtracking
 
+        :param width: width of the board
+        :param height: the height of the board
+        :return: print a single solution for the problem
+    """
     # Initialization of Board matrix
-    board = [[-1 for i in range(n)] for i in range(n)]
+    board = [[-1 for i in range(width)] for i in range(height)]
 
     # move_x and move_y define next move of Knight.
-    # move_x is for next value of x coordinate
-    # move_y is for next value of y coordinate
+    # move_x is for next value of vertical coordinate
+    # move_y is for next value of horizontal coordinate
+    # This provides all possible next positions for a knight
     move_x = [2, 1, -1, -2, -2, -1, 1, 2]
     move_y = [1, 2, 2, 1, -1, -2, -2, -1]
 
@@ -53,28 +65,37 @@ def solveKT(n):
     pos = 1
 
     # Checking if solution exists or not
-    if (not solveKTUtil(n, board, 0, 0, move_x, move_y, pos)):
+    if not solveKTUtil(width, height, board, 0, 0, move_x, move_y, pos):
         print("Solution does not exist")
     else:
-        printSolution(n, board)
+        printSolution(width, height, board)
 
 
-def solveKTUtil(n, board, curr_x, curr_y, move_x, move_y, pos):
-    '''
-        A recursive utility function to solve Knight Tour
-        problem
-    '''
+# function uses backtracking to find the answer
+def solveKTUtil(width, height, board, curr_x, curr_y, move_x, move_y, pos):
+    """
+        The backtracking function for solving the problem
 
-    if (pos == n ** 2):
+        :param width: width of the board
+        :param height: the height of the board
+        :param board: the 2D board array that would be operated upon
+        :param curr_x: the vertical index of the position to start from
+        :param curr_y: the horizontal index of the position to start from
+        :param move_x: the vertical index of the position to move to
+        :param move_y: the horizontal index of the position to move to
+        :param pos: number of cells visited already
+        :return: A boolean value. If true there is a solution, else there is no solution
+    """
+    if pos == width * height:
         return True
 
     # Try all next moves from the current coordinate x, y
     for i in range(8):
         new_x = curr_x + move_x[i]
         new_y = curr_y + move_y[i]
-        if (isSafe(new_x, new_y, board)):
+        if isSafe(new_x, new_y, board, width, height):
             board[new_x][new_y] = pos
-            if (solveKTUtil(n, board, new_x, new_y, move_x, move_y, pos + 1)):
+            if solveKTUtil(width, height, board, new_x, new_y, move_x, move_y, pos + 1):
                 return True
 
             # Backtracking
@@ -85,4 +106,8 @@ def solveKTUtil(n, board, curr_x, curr_y, move_x, move_y, pos):
 # Driver Code
 if __name__ == "__main__":
     # Function Call
-    solveKT(n)
+    if len(sys.argv) != 3:
+        print("python knights_tour.py <width> <height>")
+    else:
+        print("running")
+        solveKT(int(sys.argv[1]), int(sys.argv[2]))
